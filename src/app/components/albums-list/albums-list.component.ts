8734df4d9cf3a9cc226c87b43f27379608aab092
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2019-06-05 22:21:36 +0200

    feat(albumsList): layout update
    layout update

--- src/app/components/albums-list/albums-list.component.ts
+++ src/app/components/albums-list/albums-list.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CoreReducer } from 'src/app/store/reducers';
 import { Store } from '@ngrx/store';
 import { AlbumsStoreActions } from 'src/app/store/actions/albums/albums.actions';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { Album } from 'src/app/store/models/album/album.model';
+import { UsersStoreActions } from 'src/app/store/actions';
+import { User } from 'src/app/store/models/indx';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-albums-list',
@@ -12,12 +15,15 @@ import { Album } from 'src/app/store/models/album/album.model';
 })
 export class AlbumsListComponent implements OnInit {
   public albums$: Observable<Album[]>;
+  public users$: Observable<User[]>;
   public loaded$: Observable<boolean>;
 
   constructor(private _store: Store<CoreReducer.State>) { }
 
   ngOnInit() {
     this._store.dispatch(new AlbumsStoreActions.LoadAlbumsAction());
+    this._store.dispatch(new UsersStoreActions.LoadUsersAction());
+
     this.getAllAlbums();
     this.getLoaderState();
   }
@@ -28,7 +34,19 @@ export class AlbumsListComponent implements OnInit {
    * @returns void
    */
   getAllAlbums(): void {
-    this.albums$ = this._store.select(CoreReducer.getAll);
+    const albumsWithoutUsers$ = this._store.select(CoreReducer.getAllAlbums);
+    const users$ = this._store.select(CoreReducer.getAllUsers);
+
+    this.albums$ = combineLatest(albumsWithoutUsers$, users$).pipe(
+      map(([albums, users]) => {
+        const updatedAlbums = albums.map(album => {
+          const relatedUser = users.find(user => user.id === album.userId);
+          album.user = relatedUser;
+          return album;
+        })
+        return updatedAlbums;
+      })
+    )
   }
 
   /**
