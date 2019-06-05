76deafe937995251e72f3abe9fc08c43cc08db8b
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2019-06-05 20:32:44 +0200

    feat(albumsList): initial layout
    initial layout

--- src/app/components/albums-list/albums-list.component.ts
+++ src/app/components/albums-list/albums-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CoreReducer } from 'src/app/store/reducers';
 import { Store } from '@ngrx/store';
 import { AlbumsStoreActions } from 'src/app/store/actions/albums/albums.actions';
-import { AlbumsState } from '../../store/reducers/albums/albums.reducer';
+import { Observable } from 'rxjs';
+import { Album } from 'src/app/store/models/album/album.model';
 
 @Component({
   selector: 'app-albums-list',
@@ -10,13 +11,33 @@ import { AlbumsState } from '../../store/reducers/albums/albums.reducer';
   styleUrls: ['./albums-list.component.scss']
 })
 export class AlbumsListComponent implements OnInit {
+  public albums$: Observable<Album[]>;
+  public loaded$: Observable<boolean>;
 
   constructor(private _store: Store<CoreReducer.State>) { }
 
   ngOnInit() {
-    this._store.dispatch(new AlbumsStoreActions.LoadAlbumsAction(true));
-    // this._store.select(CoreReducer.getAll).subscribe(res => console.log('GET ALL', res))
-    // this._store.select(CoreReducer.selectAlbumsEntities).subscribe(res => console.log('GET selectAlbumsEntities', res))
+    this._store.dispatch(new AlbumsStoreActions.LoadAlbumsAction());
+    this.getAllAlbums();
+    this.getLoaderState();
+  }
+
+  /**
+   * Gets all albums
+   * 
+   * @returns void
+   */
+  getAllAlbums(): void {
+    this.albums$ = this._store.select(CoreReducer.getAll);
+  }
+
+  /**
+   * Gets album loader state
+   * 
+   * @returns void
+   */
+  getLoaderState(): void {
+    this.loaded$ = this._store.select(CoreReducer.areAlbumsLoaded);
   }
 
 }
